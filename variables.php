574095d66e9385bf9e67e2888cee3a464fb8d94a
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-03-09 16:04:48 +0100

    edit: switch users [] by fetch()

--- variables.php
+++ variables.php
@@ -1,57 +1,10 @@
 <?php
+$usersStatement = $mysqlClient->prepare('SELECT * FROM users');
+$usersStatement = $mysqlClient->execute();
+$users = $usersStatement->fetchAll();
 
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-        'password' => 'devine',
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-        'password' => 'MiamMiam',
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-        'password' => 'laCasto28',
-    ],
-    [
-        'full_name' => 'Faty Diarra',
-        'email' => 'faty.diarra@exemple.com',
-        'age' => 31,
-        'password' => 'fatyTest',
-    ],
-];
 
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'recipe' => 'Etape 1 : prenez une belle salade',
-        'author' => 'lorene.castor@exemple.com',
-        'is_enabled' => true,
-    ],
-];
-
+$recipesStatement = $mysqlClient->prepare('SELECT * FROM recipes WHERE is_enabled is TRUE');
+$recipesStatement = $mysqlClient->execute();
+$recipes = $recipesStatement->fetchAll();
 ?>
