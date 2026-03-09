8539582fc4ddc84ee444bb3d2c1acf16e426e700
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-03-09 17:10:40 +0100

    edit variable fetch()

--- variables.php
+++ variables.php
@@ -1,10 +1,12 @@
 <?php
+
+require_once(__DIR__ . '/databaseconnect.php');  
+
 $usersStatement = $mysqlClient->prepare('SELECT * FROM users');
-$usersStatement = $mysqlClient->execute();
+$usersStatement->execute();
 $users = $usersStatement->fetchAll();
 
-
 $recipesStatement = $mysqlClient->prepare('SELECT * FROM recipes WHERE is_enabled is TRUE');
-$recipesStatement = $mysqlClient->execute();
+$recipesStatement->execute();
 $recipes = $recipesStatement->fetchAll();
 ?>
