cf4e73c22b8337b735b2ea5f2f903b68e7262bea
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-03-09 11:19:10 +0100

    create new user

--- variables.php
+++ variables.php
@@ -5,16 +5,25 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'devine',
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'MiamMiam',
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => 'laCasto28',
+    ],
+    [
+        'full_name' => 'Faty Diarra',
+        'email' => 'faty.diarra@exemple.com',
+        'age' => 31,
+        'password' => 'fatyTest',
     ],
 ];
 
