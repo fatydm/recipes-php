5c057892efdb2f8cad481ce7b1323824151e0c58
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-03-25 17:31:25 +0100

    created post crud

--- recipes_post_create.php
+++ recipes_post_create.php
@@ -7,3 +7,53 @@ require_once(__DIR__ . '/databaseconnect.php');
 
 $postData = $_POST;
 
+if(!isset($_POST['title'])
+    || !isset($_POST['recipe'])) 
+{
+    echo 'Il faut entrer un titre et une recette pour valider le formulaire';
+    return;
+}
+
+$title = $_POST['title'];
+$recipe = $_POST['recipe'];
+
+$insertRecipe = $mysqlClient->prepare('INSERT INTO recipes(title, recipe, author, is_enabled) VALUES (:title, :recipe, :author, :is_enabled)');
+$insertRecipe->execute([
+    'title' => $title, 
+    'recipe' => $recipe, 
+    'author' => $_SESSION['LOGGED_USER']['email'],
+    'is_enabled' => 1,
+]);
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Recettes - Création de recette</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+        <?php require_once(__DIR__ . '/header.php'); ?>
+        <!-- MESSAGE DE SUCCES -->
+        <h1>Recette ajoutée avec succès !</h1>
+
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $title ; ?></h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_SESSION['LOGGED_USER']['email']; ?></p>
+                <p class="card-text"><b>Recette</b> : <?php echo $recipe; ?></p>
+            </div>
+        </div>
+    </div>
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
